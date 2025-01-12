086ffde5a756b0f4b5530e35c7a3f963186ca7da
kruasanow <rusanow.mitya@bk.ru>
2025-01-12 22:04:25 +0300

    123123

--- static/js/devjs/ajaxapi.js
+++ static/js/devjs/ajaxapi.js
@@ -1,29 +1,33 @@
-// function apiCarousel() {
-//   window.onload = function () {
-//     fetch("/api/getevents")
-//       .then((response) => response.json())
-//       .then((data) => {
-//         const carousel = document.getElementById("card-carousel");
-
-//         // Убедимся, что контейнер пуст перед добавлением новых карточек
-//         carousel.innerHTML = '';
-
-//         data.forEach((item) => {
-//           const card = document.createElement("div");
-//           card.className = "card";
-//           card.innerHTML = `
-//             <div class="image-container" style="background-image: url('{{ url_for('static', filename='upload/' + item.title + '/' + item.image_url) }}');"></div>
-//             <p>${item.title}</p>
-//             <p>${item.description}</p>
-//             <p>${item.date}</p>
-//           `;
-//           carousel.appendChild(card);
-//         });
-
-//         // После добавления карточек, инициализируем карусель
-//         const carouselInstance = new CardCarousel(carousel);
-//       })
-//       .catch((error) => console.error("Error loading items:", error));
-//   };
-// }
-// apiCarousel();
+function apiCarousel() {
+    window.onload = function () {
+      fetch("/api/getevents")
+        .then((response) => response.json())
+        .then((data) => {
+          const carousel = document.getElementsByClassName("list")[0];
+  
+          // Очистка контейнера перед добавлением новых элементов (если нужно)
+          // carousel.innerHTML = '';
+  
+          data.forEach((item, index) => {
+            const card = document.createElement("li");
+            card.className = 'item';
+  
+            card.innerHTML = `
+              <input type="radio" id="radio_testimonial-${index + 1}" name="basic_carousel" ${index === 0 ? 'checked' : ''} />
+              <label class="label_testimonial-${index + 1}" for="radio_testimonial-${index + 1}">${item.title}</label>
+              <div class="content-test content_testimonial-${index + 1}">
+                <span class="picto"></span>
+                <h1>${item.title}</h1>
+                <p>“${item.description}”</p>
+                <p class="testimonialFrom">Kruasanow</p>
+                <p class="testimonialState">${item.date}</p>
+              </div>
+            `;
+            carousel.appendChild(card);
+          });
+        })
+        .catch((error) => console.error("Error loading items:", error));
+    };
+  }
+  apiCarousel();
+  
